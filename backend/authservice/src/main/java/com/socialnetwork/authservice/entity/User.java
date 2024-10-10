5bbea741e46a2db161b625e2348bb7ce13ea92dc
Venetian Doge <elvagyodasz@gmail.com>
2024-10-10 15:50:23 +0300

    Implement UserDetails in User

--- backend/authservice/src/main/java/com/socialnetwork/authservice/entity/User.java
+++ backend/authservice/src/main/java/com/socialnetwork/authservice/entity/User.java
@@ -2,11 +2,17 @@ package com.socialnetwork.authservice.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +25,36 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+
+    // UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
