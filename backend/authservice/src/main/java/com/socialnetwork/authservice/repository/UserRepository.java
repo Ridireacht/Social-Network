73303534ebf056685fcab314c291665babce185d
Venetian Doge <elvagyodasz@gmail.com>
2024-10-10 15:38:54 +0300

    Add repository

---
+++ backend/authservice/src/main/java/com/socialnetwork/authservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.socialnetwork.authservice.repository;
+
+import com.socialnetwork.authservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
