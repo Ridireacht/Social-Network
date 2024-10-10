dbfee9d875418004b2f91ae16b346f04679d4c9d
Venetian Doge <elvagyodasz@gmail.com>
2024-10-10 15:37:31 +0300

    Add User entity

---
+++ backend/authservice/src/main/java/com/socialnetwork/authservice/entity/User.java
@@ -0,0 +1,18 @@
+package com.socialnetwork.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+    private String password;
+    private String role;
+}
